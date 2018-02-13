e93e2c0aa95d28934981a3d367747c32bb318424
Narzhan <vasek.bruzek@gmail.com>
2018-02-13 14:15:08 +0100

    Bug fix and testing cert conf

--- lr_agent_app.py
+++ lr_agent_app.py
@@ -8,26 +8,38 @@ from lr_agent_client import LRAgentClient
 from exception.exc import InitException
 from loggingtools.logger import build_logger
 
-WHALEBONE_LR_CLIENT_CERT = os.environ['WHALEBONE_LR_CLIENT_CERT']
-WHALEBONE_PORTAL_ADDRESS = os.environ['WHALEBONE_PORTAL_ADDRESS']
 
+# password for cert in env
 
 
 def validate_settings():
-    if not WHALEBONE_LR_CLIENT_CERT:
+    try:
+        client_cert = os.environ['WHALEBONE_LR_CLIENT_CERT']
+    except KeyError:
         raise InitException('System env WHALEBONE_LR_CLIENT_CERT must be set')
-    if not os.path.exists(WHALEBONE_LR_CLIENT_CERT) or os.stat(WHALEBONE_LR_CLIENT_CERT).st_size == 0:
-        raise InitException('Client certificate {0} must exist and mustn\'t be empty'.format(WHALEBONE_LR_CLIENT_CERT))
-    if not WHALEBONE_PORTAL_ADDRESS:
+    try:
+        proxy_address = os.environ['WHALEBONE_PORTAL_ADDRESS']
+    except KeyError:
         raise InitException('System env WHALEBONE_PORTAL_ADDRESS must be set')
+    try:
+        client_cert_pass = os.environ['WHALEBONE_LR_CLIENT_CERT_PASS']
+    except KeyError:
+        client_cert_pass = "password" # remove for prodution
+        # raise InitException('System env WHALEBONE_LR_CLIENT_CERT_PASS must be set')
+
+    if not os.path.exists(client_cert) or os.stat(client_cert).st_size == 0:  # change to None or len()=0
+        raise InitException('Client certificate {0} must exist and mustn\'t be empty'.format(client_cert))
+    return client_cert, proxy_address, client_cert_pass
 
 
 async def connect():
-    sslContext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    sslContext.load_cert_chain(WHALEBONE_LR_CLIENT_CERT)
+    client_cert, proxy_address, client_cert_pass = validate_settings()
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(client_cert, password=client_cert_pass)
+    # sslContext.load_cert_chain(WHALEBONE_LR_CLIENT_CERT)
     logger = logging.getLogger(__name__)
-    logger.info("Connecting to {0}".format(WHALEBONE_PORTAL_ADDRESS))
-    return await websockets.connect(WHALEBONE_PORTAL_ADDRESS, ssl=sslContext)
+    logger.info("Connecting to {0}".format(proxy_address))
+    return await websockets.connect(proxy_address, ssl=ssl_context)
 
 
 async def local_resolver_agent_app():
@@ -52,13 +64,12 @@ async def local_resolver_agent_app():
 
 
 if __name__ == '__main__':
-    if os.environ['LOGGING_LEVEL'] == "DEBUG":
+    if 'LOGGING_LEVEL' in os.environ:
         logging.basicConfig(level=logging.DEBUG)
         logger = logging.getLogger(__name__)
     else:
         logger = build_logger(__name__, "/home/narzhan/Downloads/agent_logs/")
     try:
-        validate_settings()
         loop = asyncio.get_event_loop()
         loop.run_until_complete(local_resolver_agent_app())
         loop.run_forever()

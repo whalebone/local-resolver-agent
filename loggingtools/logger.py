b448e458b9561550e8b6f7d9f732938a0dfc133d
narzhan <vasek.bruzek@gmail.com>
2018-04-15 15:30:36 +0200

    'Agent' prefix for all logs

--- loggingtools/logger.py
+++ loggingtools/logger.py
@@ -12,7 +12,7 @@ def build_logger(name: str, log_path: str, log_level: str = "INFO"):
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
 
-    handler = RotatingFileHandler("{}/{}.log".format(log_path, name), maxBytes=20000000, backupCount=12)
+    handler = RotatingFileHandler("{}/agent-{}.log".format(log_path, name), maxBytes=20000000, backupCount=12)
     handler.setLevel(log_level)
 
     formatter = logging.Formatter('%(asctime)s | %(lineno)d | %(levelname)s | %(message)s')
--- lr_agent_app.py
+++ lr_agent_app.py
@@ -34,13 +34,13 @@ async def connect():
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain(client_cert)
     # sslContext.load_cert_chain(WHALEBONE_LR_CLIENT_CERT)
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("main")
     logger.info("Connecting to {0}".format(proxy_address))
     return await websockets.connect(proxy_address, ssl=ssl_context)
 
 
 async def local_resolver_agent_app():
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("main")
     try:
         interval = int(os.environ['PERIODIC_INTERVAL'])
     except KeyError:
@@ -68,9 +68,9 @@ async def local_resolver_agent_app():
 if __name__ == '__main__':
     if 'LOGGING_LEVEL' in os.environ:
         logging.basicConfig(level=logging.DEBUG)
-        logger = logging.getLogger(__name__)
+        logger = logging.getLogger("main")
     else:
-        logger = build_logger(__name__, "/etc/whalebone/logs/")
+        logger = build_logger("main", "/etc/whalebone/logs/")
     try:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(local_resolver_agent_app())

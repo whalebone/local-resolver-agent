604bca8a64aa60e1e2d86371a5a33349999c19f7
Narzhan <vasek.bruzek@gmail.com>
2018-10-11 11:09:21 +0200

    Method arguments type annotations

--- dockertools/compose_parser.py
+++ dockertools/compose_parser.py
@@ -7,14 +7,14 @@ SUPPORTED_VERSIONS = ['1', '3']
 
 class ComposeParser:
 
-    def create_service(self, compose_yaml):
+    def create_service(self, compose_yaml: str) -> dict:
         compose = self.parse(compose_yaml)
         self.validate(compose)
         for service_name in compose['services']:
             compose['services'][service_name]["name"] = service_name
         return compose
 
-    def parse(self, compose_yaml):
+    def parse(self, compose_yaml: str) -> dict:
         try:
             parsed_compose = yaml.load(compose_yaml)
             if 'version' in parsed_compose:
@@ -28,7 +28,7 @@ class ComposeParser:
         except yaml.YAMLError as e:
             raise ComposeException("Invalid compose YAML format {}".format(e))
 
-    def validate(self, parsed_compose):
+    def validate(self, parsed_compose: str):
         if parsed_compose['version'] not in SUPPORTED_VERSIONS:
             raise ComposeException("Compose version '{0}' not supported. Supported versions: {1}"
                                    .format(parsed_compose['version'], SUPPORTED_VERSIONS))

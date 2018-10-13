e923c3fb5069d40424b0e8ac7fb0bb9c99803148
Narzhan <vasek.bruzek@gmail.com>
2018-10-13 10:59:44 +0200

    Periodic flow keeps error messages in one sysinfo

--- lr_agent_app.py
+++ lr_agent_app.py
@@ -54,8 +54,8 @@ async def local_resolver_agent_app():
             # else:
             #     await local_client.start_api()
             while True:
-                await remote_client.validate_host()
                 await remote_client.send_sys_info()
+                await remote_client.validate_host()
                 await asyncio.sleep(interval)
         except Exception as e:
             logger.error('Generic error: {}'.format(str(e)))

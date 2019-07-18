a2a18b3c204b4e55492967ab209d87aef5cadcd1
Narzhan <vasek.bruzek@gmail.com>
2019-07-18 10:14:12 +0200

    Fixed multi line logging and max file creation

--- loggingtools/logger.py
+++ loggingtools/logger.py
@@ -11,12 +11,12 @@ def build_logger(name: str, log_path: str, log_level: str = "INFO"):
 
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
+    if not logger.handlers:
+        handler = RotatingFileHandler("{}/agent-{}.log".format(log_path, name), maxBytes=20000000, backupCount=12)
+        handler.setLevel(log_level)
 
-    handler = RotatingFileHandler("{}/agent-{}.log".format(log_path, name), maxBytes=20000000, backupCount=12)
-    handler.setLevel(log_level)
-
-    formatter = logging.Formatter('%(asctime)s | %(lineno)d | %(levelname)s | %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+        formatter = logging.Formatter('%(asctime)s | %(lineno)d | %(levelname)s | %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     return logger

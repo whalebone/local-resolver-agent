c85aefa43fcf402da335857b7ab21681955ea828
Narzhan <vasek.bruzek@gmail.com>
2019-10-31 12:04:13 +0100

    Console handler logs are formatted using standard formatter

--- loggingtools/logger.py
+++ loggingtools/logger.py
@@ -12,16 +12,17 @@ def build_logger(name: str, log_path: str, log_level: str = "INFO"):
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     if not logger.handlers:
-        if "DISABLE_FILE_LOGS" not in os.environ:
-            handler = RotatingFileHandler("{}/agent-{}.log".format(log_path, name), maxBytes=20000000, backupCount=12)
-            handler.setLevel(log_level)
-
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(log_level)
+        # console_handler.setLevel(log_level)
 
         formatter = logging.Formatter('%(asctime)s | %(lineno)d | %(levelname)s | %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        if "DISABLE_FILE_LOGS" not in os.environ:
+            handler = RotatingFileHandler("{}/agent-{}.log".format(log_path, name), maxBytes=20000000, backupCount=12)
+            # handler.setLevel(log_level)
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+        console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
     return logger

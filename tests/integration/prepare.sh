83decb347051dfab913eb911de306b62d4b23cd1
Narzhan <vasek.bruzek@gmail.com>
2020-05-18 10:41:55 +0200

    Added hosts record for wsproxy

--- tests/integration/prepare.sh
+++ tests/integration/prepare.sh
@@ -5,4 +5,5 @@ rm /etc/resolv.conf
 ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 systemctl disable systemd-resolved.service
 service systemd-resolved stop
+echo "127.0.0.1 tmp-test" >> /etc/hosts
 docker-compose up -d

6116a8a735d1607d1470a6b0969991cc64714c61
Narzhan <vasek.bruzek@gmail.com>
2020-06-05 09:59:26 +0200

    Stats diff computation changed

--- sysinfo/sys_info.py
+++ sysinfo/sys_info.py
@@ -172,11 +172,22 @@ class SystemInfo:
                 except FileNotFoundError:
                     return results
                 else:
-                    stats = {stat: value - previous[stat] for stat, value in results.items() if stat in previous}
-                    if any(stat_value < 0 for stat_value in stats.values()):
-                        return {}
-                    else:
-                        return stats
+                    pattern = re.compile(r"answer.*|query.*|request.*")
+                    stats = {}
+                    for stat, value in results.items():
+                        if stat in previous:
+                            if pattern.match(stat):
+                                diff = value - previous[stat]
+                                if diff >= 0:
+                                    stats[stat] = diff
+                            else:
+                                stats[stat] = value
+                    return stats
+                    # stats = {stat:  value - previous[stat] for stat, value in results.items() if stat in previous}
+                    # if any(stat_value < 0 for stat_value in stats.values()):
+                    #     return {}
+                    # else:
+                    #     return stats
                 finally:
                     self.result_manipulation("w", results)
         except Exception as e:

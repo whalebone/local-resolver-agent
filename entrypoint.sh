27f2b090159a81fd800839f2d1195fad0936648e
Narzhan <vasek.bruzek@gmail.com>
2020-10-09 12:48:14 +0200

    User agent added to docker socket permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,19 @@ CURRENT_UID=${uid:-9999}
 # Notify user about the UID selected
 echo "Current UID : $CURRENT_UID"
 # Create user called "docker" with selected UID
-useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m docker
+useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m agent
 # Set "HOME" ENV variable for user's home directory
-export HOME=/home/docker
+export HOME=/home/agent
 
-chown docker -R /etc/whalebone/  && chmod ug+rwxs -R /etc/whalebone/
-chown docker -R /opt/agent/  && chmod ug+rwxs -R /opt/agent/
+SOCK_DOCKER_GID=`ls -ng /var/run/docker.sock | cut -f3 -d' '`
+
+if ! groups agent | grep -q docker; then
+  groupadd -g ${SOCK_DOCKER_GID} -o hostdocker
+  usermod -a -G hostdocker agent
+fi
+
+chown agent -R /etc/whalebone/  && chmod ug+rwxs -R /etc/whalebone/
+chown agent -R /opt/agent/  && chmod ug+rwxs -R /opt/agent/
 
 # Execute process
-exec /usr/local/bin/gosu docker "$@"
+exec /usr/local/bin/gosu agent "$@"

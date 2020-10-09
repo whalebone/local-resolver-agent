cdb39ea9a44772013d8d7733907b5efc3a1fd34a
Narzhan <vasek.bruzek@gmail.com>
2020-10-09 13:44:24 +0200

    Echo uid print removed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 # If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 9999 as default
 CURRENT_UID=${uid:-9999}
 
-# Notify user about the UID selected
-echo "Current UID : $CURRENT_UID"
 # Create user called "docker" with selected UID
 useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m agent
 # Set "HOME" ENV variable for user's home directory
